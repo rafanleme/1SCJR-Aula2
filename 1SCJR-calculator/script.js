74138ca5dd56ff3a26e1dc44be6f977626ae1e71
rafanleme <rafanleme@gmail.com>
2023-02-09 09:09:30 -0300

    commit inicial

---
+++ 1SCJR-calculator/script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById('display');
+
+let newNumber = true;
+let operator;
+let previousNumber;
+
+function updateDisplay(numero) {}
+
+const insertNumber = (event) => {}
+
+const selectOperator = (event) => {}
+
+const calculate = () => {}
+
+const equal = document.querySelector("#igual");
+
+equal.addEventListener('click', calculate);
+
+const clearDisplay = () => display.textContent = "";
+
+document.querySelector("#limparDisplay").addEventListener("click", clearDisplay);
+
+const clearCalc = () => {};
+
+document.querySelector("#limparCalculo").addEventListener("click", clearCalc);
+
+const removeLastNumber = () => {};
+
+document.querySelector("#apagar").addEventListener("click", removeLastNumber);
+
+const invertSignal = () => {}
+
+document.querySelector("#inverter").addEventListener("click", invertSignal);
